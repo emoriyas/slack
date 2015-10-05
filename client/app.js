2a5e3d5a066fbabdf3f7d6afb33bacdf0c97ee9d
emoriyas <ehddnr85963@gmail.com>
2015-10-05 10:11:03 -1000

    slack
    slack

---
+++ client/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Eric on 10/5/2015.
+ */
+Template.messages.helpers({
+    messages: [
+        { text: "All these messages" },
+        { text: "Uses the same template" },
+        { text: "But have a different data context" },
+        { text: "It's why these messages are all different!" },
+        { text: "Hey!" },
+        { text: "What's up man!" },
+        { text: "Hello" }
+    ]
+});
